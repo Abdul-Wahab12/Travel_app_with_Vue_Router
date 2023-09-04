872092860f23859122d382009b1063358c1f36fa
abdul wahab <abdul.wahab@codeforesight.com>
2023-09-04 17:18:44 +0500

    Added: add route transition in app component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <TheNavigation />
   <div class="container">
-    <router-view :key="$route.path"></router-view>
+    <router-view v-slot="{Component}">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -14,6 +18,55 @@ export default {
   }
 }
 </script>
+
+<style lang="css">
+
+/* Fade transition */
+
+  .fade-enter-active, 
+  .fade-leave-active {
+    transition: opacity 0.3s;
+  }
+
+  .fade-enter,
+  .fade-leave-to{
+    opacity: 0;
+  }
+
+/* Move Up transition */
+
+  /* .moveUp-enter-active {
+    animation: fadeIn 1s ease-in;
+  }
+
+  @keyframes fadeIn {
+    0% { opacity: 0; }
+    50% { opacity: 0.5; }
+    100% { opacity: 1; }
+  }
+
+  .moveUp-leave-active {
+    animation: moveUp 0.3s ease-in;
+  }
+
+  @keyframes moveUp {
+    0% { transform: translateY(0); }
+    100% { transform: translateY(-400px); }
+  } */
+
+  /* Slide transition */
+
+  /* .slide-enter-active,
+  .slide-leave-active {
+    transition: opacity 1s, transform 1s;
+  } 
+
+  .slide-enter-from,
+  .slide-leave-to {
+    opacity: 0;
+    transform: translateX(-30%);
+  } */
+</style>
 
 
 
