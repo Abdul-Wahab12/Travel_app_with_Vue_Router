25550e25b065390951083ebc7b07ce4ed4142b6b
abdul wahab <abdul.wahab@codeforesight.com>
2023-09-04 18:13:52 +0500

    Added: add route or navigation guard in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory} from 'vue-router';
 import Home from '../views/Home.vue';
+import sourceData from '../data.json';
 // import NotFound from '../views/NotFound.vue';
 
 
@@ -17,6 +18,19 @@ const routes =  [
             ...route.params,
             id: parseInt(route.params.id)
         }),
+        beforeEnter(to, from) {
+            const exists = sourceData.destinations.find((destination) => destination.id === parseInt(to.params.id));
+
+            if(!exists) {
+                return {
+                    name: 'NotFound',
+                    // allows keeping the URL while rendering a different page
+                    params: { pathMatch: to.path.split('/').slice(1) },
+                    query: to.query,
+                    hash: to.hash,
+                }
+            }
+        },
         children: [
             {
                 path: ':experienceSlug',
